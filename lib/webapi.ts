40fbafb8a867fe7d045a0a2b159138172daeec00
Brom127 <peter.leicht32@gmail.com>
2024-12-12 10:53:37 +0100

    add user agent to fetch requests

--- lib/webapi.ts
+++ lib/webapi.ts
@@ -2,7 +2,11 @@ import fetch from "node-fetch";
 
 type QueryString = string[][] | Record<string, string> | URLSearchParams;
 export async function webAPI(url: string, query?: QueryString, parse_json = false): Promise<any> {
-	const resp = await fetch(url + "?" + new URLSearchParams(query));
+	const resp = await fetch(url + "?" + new URLSearchParams(query), {
+		headers: {
+			"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36",
+		},
+	});
 
 	if (!resp.ok) {
 		throw new Error(`HTTP ${resp.status} ${resp.statusText}`);
